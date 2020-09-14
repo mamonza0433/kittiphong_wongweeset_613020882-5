bed158ae5f47ae49446c7fc9af5c69386bb2727a
Kittiphong_wongweeset <61855764+mamonza0433@users.noreply.github.com>
2020-09-14 13:04:16 +0700

    yes

--- Ass5_Sec1_613020882-5/Employee.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ass5
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Employee (val name:String,val gender : String,val email : String, val salary: Int) : Parcelable {
-
-}
